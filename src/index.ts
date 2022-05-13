0461f869448f7520318bc8b6cc79f8958483ea93
jimao <jimao@jimaodeMBP.lan>
2022-05-14 00:23:26 +0800

    套利机会

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,13 @@ async function main() {
     //   defaultType: 'future',
     // },
   });
-  const watcher = new TickerWatcher(binance, ['BTC/USDT', 'LINK/USDT', 'LINK/BTC'], (ticker) => {
-    const a = ticker['BTC/USDT'].close as number;
-    const b = ticker['LINK/USDT'].close as number;
-    const c = ticker['LINK/BTC'].close as number;
-    console.log(a, b, c, b / a);
+  const watcher = new TickerWatcher(binance, ['DOGE/USDT', 'SHIB/USDT', 'SHIB/DOGE'], (ticker) => {
+    const a = ticker['DOGE/USDT'].close as number;
+    const b = ticker['SHIB/USDT'].close as number;
+    const c = ticker['SHIB/DOGE'].close as number;
+    const d = a * c;
+    const e = (d - b) / b;
+    console.log(b, d, e * 100);
   }, 1000);
   watcher.Start();
 }

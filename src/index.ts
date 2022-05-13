6dac8d5910aabb90008038905d7afa5500f640f2
jimao <jimao@jimaodeMBP.lan>
2022-05-13 23:50:11 +0800

    计算

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,11 @@ async function main() {
     //   defaultType: 'future',
     // },
   });
-  const watcher = new TickerWatcher(binance, ['BTC/USDT', 'ADA/USDT', 'ADA/BTC'], (ticker) => {
-    console.log(
-      'BTC/USDT', ticker['BTC/USDT'].close,
-      'ADA/USDT', ticker['ADA/USDT'].close,
-      'ADA/BTC', ticker['ADA/BTC'].close,
-    );
+  const watcher = new TickerWatcher(binance, ['BTC/USDT', 'LINK/USDT', 'LINK/BTC'], (ticker) => {
+    const a = ticker['BTC/USDT'].close as number;
+    const b = ticker['LINK/USDT'].close as number;
+    const c = ticker['LINK/BTC'].close as number;
+    console.log(a, b, c, b / a);
   }, 1000);
   watcher.Start();
 }

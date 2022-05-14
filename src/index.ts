4fc2e69b6fe38b7e4bd10f992e0675a725e8c4f0
jimao <jimao@jimaodeMBP.lan>
2022-05-15 00:01:55 +0800

    ws提交

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import ccxt from 'ccxt';
 import { TickerWatcher } from './ticker_watcher';
 import { Robot } from './robot';
+const { Spot } = require('@binance/connector')
 
 async function main() {
 
+  const client = new Spot('', '', {
+    wsURL: 'wss://testnet.binance.vision' // If optional base URL is not provided, wsURL defaults to wss://stream.binance.com:9443
+  })
 
+  const callbacks = {
+    open: () => client.logger.log('open'),
+    close: () => client.logger.log('closed'),
+    message: (data: any) => client.logger.log(data)
+  }
+  const aggTrade = client.aggTradeWS('bnbusdt', callbacks)
+
+  // unsubscribe the stream above
+  setTimeout(() => client.unsubscribe(aggTrade), 3000)
+
+  // support combined stream
+  const combinedStreams = client.combinedStreams(['btcusdt@miniTicker', 'ethusdt@tikcer'], callbacks)
 
 
 

a947c087447f4fde9ba999ee0dd481f4760bea4a
jimao <jimao@jimaodeMBP.lan>
2022-05-15 23:17:19 +0800

    提交

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,33 @@
 import ccxt from 'ccxt';
 import { TickerWatcher } from './ticker_watcher';
 import { Robot } from './robot';
-const { Spot } = require('@binance/connector')
+const { Spot } = require('@binance/connector');
+import fs from 'fs';
 
 async function main() {
 
-  const client = new Spot('', '', {
-    wsURL: 'wss://testnet.binance.vision' // If optional base URL is not provided, wsURL defaults to wss://stream.binance.com:9443
-  })
+  const client = new Spot()
 
   const callbacks = {
     open: () => client.logger.log('open'),
     close: () => client.logger.log('closed'),
-    message: (data: any) => client.logger.log(data)
+    message: (data: any) => {
+      console.log(data);
+      const jsonObject = JSON.parse(data);
+      fs.writeFileSync('1.json', JSON.stringify(jsonObject, null, 2));
+      // const jsonObject = JSON.parse(data);
+      // console.log(jsonObject.p);
+    },
   }
-  const aggTrade = client.aggTradeWS('bnbusdt', callbacks)
+  // client.aggTradeWS('btcusdt', callbacks)
+  // client.partialBookDepth('btcusdt', 5, '1000ms', callbacks);
+  client.bookTickerWS('btcusdt', callbacks);
 
   // unsubscribe the stream above
-  setTimeout(() => client.unsubscribe(aggTrade), 3000)
+  // setTimeout(() => client.unsubscribe(aggTrade), 3000)
 
   // support combined stream
-  const combinedStreams = client.combinedStreams(['btcusdt@miniTicker', 'ethusdt@tikcer'], callbacks)
+  // const combinedStreams = client.combinedStreams(['btcusdt@miniTicker', 'ethusdt@tikcer'], callbacks)
 
 
 

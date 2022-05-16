f669f0beee782c41e93c3d627a062a27908f37a4
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-16 13:15:52 +0800

    重写book

--- src/book.ts
+++ src/book.ts
@@ -2,47 +2,22 @@
 export
 class Book {
   public constructor(
-    private bid: [number, number][] = [],
-    private ask: [number, number][] = [],
+    private orders: [number, number][],
   ) { }
 
-  public get BID() {
-    return this.bid;
+  public get BestOrder() {
+    return this.orders[0];
   }
 
-  public get BIDBest() {
-    return this.bid[0];
+  public get BestPrice() {
+    return this.orders[0][0];
   }
 
-  public get BIDBestPrice() {
-    return this.bid[0][0];
+  public get BestVolume() {
+    return this.orders[0][1];
   }
 
-  public get BIDBestVolume() {
-    return this.bid[0][1];
-  }
-
-  public get ASK() {
-    return this.ask;
-  }
-
-  public get ASKBest() {
-    return this.ask[0];
-  }
-
-  public get ASKBestPrice() {
-    return this.ask[0][0];
-  }
-
-  public get ASKBestVolume() {
-    return this.ask[0][1];
-  }
-
-  public Update(
-    bid: [number, number][],
-    ask: [number, number][],
-  ) {
-    this.bid = bid;
-    this.ask = ask;
+  public Update(orders: [number, number][]) {
+    this.orders = orders;
   }
 }

bd00f52f418ba623f7928c871b3155c610ccad42
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-16 16:12:36 +0800

    update

--- src/book_store.ts
+++ src/book_store.ts
@@ -15,14 +15,17 @@ class BookStore {
     book_time = -1,
   ) {
     if (!this.store.has(code[0])) {
-      this.store.set(code[0], new Map<string, Book>());
-    }
-    const book_map = this.store.get(code[0]);
-    if (!book_map?.has(code[1])) {
-      book_map?.set(code[1], new Book(orders, book_time));
+      this.store.set(code[0], new Map<string, Book>([
+        [code[1], new Book(orders, book_time)],
+      ]));
     } else {
-      const book = book_map?.get(code[1]);
-      book?.Update(orders, book_time);
+      const book_map = this.store.get(code[0]);
+      if (!book_map?.has(code[1])) {
+        book_map?.set(code[1], new Book(orders, book_time));
+      } else {
+        const book = book_map?.get(code[1]);
+        book?.Update(orders, book_time);
+      }
     }
   }
 }

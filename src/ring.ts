e6a25af345ca57dfc919d60e21872d1dbf903d15
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-17 18:02:27 +0800

    ring类

---
+++ src/ring.ts
@@ -0,0 +1,10 @@
+import { Market } from './market';
+
+export
+class Ring {
+  public constructor(
+    private readonly base_market1: Market,
+    private readonly base_market2: Market,
+    private readonly swap_market2: Market,
+  ) { }
+}

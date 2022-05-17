965c346a8fd327c823c90ce24638aba6e65e342c
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-17 12:26:16 +0800

    提交

--- src/robot.ts
+++ src/robot.ts
@@ -12,20 +12,33 @@ class Robot {
   public constructor(
     private readonly client: any,
     private readonly base: string,
-    private readonly coins: string[],
+    private readonly coins: string[] = [],
   ) { }
 
   private markets: IMarket[] = [];
+  private market_all_coins: string[] = [];
 
-  private async loadMarkets() {
+  private get_market_all_coins() {
+    const result: string[] = [];
+    this.markets.forEach((market) => {
+      result.push(market.baseAsset);
+      result.push(market.quoteAsset);
+    });
+    return Array.from(new Set(result));
+  }
+
+  private async load_markets() {
     const rsp = await this.client.exchangeInfo();
     return (rsp.data.symbols as IMarket[])
       .filter((market) => market.status === 'TRADING');
   }
 
+  private watch_markets: string[] = [];
+
   public async Start() {
-    this.markets = await this.loadMarkets();
-    console.log(this.markets.length);
+    this.markets = await this.load_markets();
+    this.market_all_coins = this.get_market_all_coins();
+    console.log(this.market_all_coins.length);
     // console.log(this.markets[0]);
   }
 }

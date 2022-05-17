91a19c05b992ec1a93625e466abe08879c6cdf45
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-17 18:02:44 +0800

    ring类

--- src/ring.ts
+++ src/ring.ts
@@ -5,6 +5,6 @@ class Ring {
   public constructor(
     private readonly base_market1: Market,
     private readonly base_market2: Market,
-    private readonly swap_market2: Market,
+    private readonly swap_market: Market,
   ) { }
 }

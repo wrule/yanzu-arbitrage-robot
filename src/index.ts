1ee896868e539a9b7dd480d69be86197f2dff5f8
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-17 11:41:29 +0800

    删除所有机器人

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,6 @@
-import ccxt from 'ccxt';
-import { TickerWatcher } from './ticker_watcher';
-import { Robot } from './robot';
-import { Spot } from '@binance/connector';
-import fs from 'fs';
 
 async function main() {
-  // client.partialBookDepth('btcusdt', 5, '1000ms', callbacks);
-  // client.bookTickerWS('btcusdt', callbacks);
-  // client.diffBookDepth('ethusdt', '100ms', callbacks);
-  // client.partialBookDepth('btcusdt', 5, '100ms', callbacks);
-
-  // unsubscribe the stream above
-  // setTimeout(() => client.unsubscribe(aggTrade), 3000)
-
-  // support combined stream
-  // const combinedStreams = client.combinedStreams(['btcusdt@miniTicker', 'ethusdt@tikcer'], callbacks)
-
-  const binance = new ccxt.binance({
-    // apiKey: Secret.API_KEY,
-    // secret: Secret.SECRET_KEY,
-    // enableRateLimit: true,
-    // options: {
-    //   defaultType: 'future',
-    // },
-  });
-  const client = new Spot();
-  const robot = new Robot(
-    binance,
-    client,
-    'usdt',
-    ['btc', 'eth', 'ada', 'link', 'uni', 'dent', 'fil'],
-  );
-  robot.Start();
+  console.log('你好，世界');
 }
 
 main();
--- src/robot.ts
+++ src/robot.ts
@@ -1,55 +1,9 @@
-import ccxt from 'ccxt';
 
 export
 class Robot {
   public constructor(
-    private readonly exchange: ccxt.binance,
     private readonly client: any,
     private readonly base: string,
     private readonly coins: string[],
   ) { }
-
-  private possibleSymbols() {
-    const result: string[] = [];
-    result.push(...(
-      this.coins.map((coin) => `${coin}/${this.base}`.toUpperCase())
-    ));
-    for (let i = 0; i < this.coins.length - 1; ++i) {
-      for (let j = i + 1; j < this.coins.length; ++j) {
-        result.push(`${this.coins[i]}/${this.coins[j]}`.toUpperCase());
-        result.push(`${this.coins[j]}/${this.coins[i]}`.toUpperCase());
-      }
-    }
-    return Array.from(new Set(result));
-  }
-
-  private async legalSymbols() {
-    const markets = await this.exchange.fetchMarkets();
-    const market_symbols = markets.map((market) => market.symbol.toUpperCase());
-    const possible_symbols = this.possibleSymbols();
-    const legal_symbols = possible_symbols.filter((symbol) => market_symbols.includes(symbol));
-    return legal_symbols;
-  }
-
-  public async Start() {
-    const legal_symbols = await this.legalSymbols();
-    console.log(legal_symbols);
-    const callbacks = {
-      open: () => this.client.logger.log('open'),
-      close: () => this.client.logger.log('closed'),
-      message: (data: any) => {
-        const jsonObject = JSON.parse(data);
-        console.log(jsonObject.data);
-      },
-    };
-    this.client.combinedStreams(['btcusdt@depth@100ms'], callbacks);
-    // this.client.aggTradeWS('linkusdt', callbacks);
-    // legal_symbols.forEach((symbol) => {
-    //   this.client.aggTradeWS(symbol.replace('/', ''), callbacks);
-    // });
-  }
-
-  public Stop() {
-
-  }
 }

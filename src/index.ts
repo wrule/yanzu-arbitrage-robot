5e24825b4311225e6b4394820abff3d4a326cb82
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-19 17:55:17 +0800

    买入粗偶

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Spot } from '@binance/connector';
 import { Robot } from './robot';
+import secret from './.secret.json';
 
 async function main() {
   console.log('你好，世界');
-  const client = new Spot({ baseURL: 'https://api2.binance.com' });
+  const client = new Spot(secret.API_KEY, secret.SECRET_KEY, { baseURL: 'https://api2.binance.com' });
   const robot = new Robot(
     client,
     'USDT',

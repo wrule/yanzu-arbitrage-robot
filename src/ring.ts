1c074ee57f2ca65b7fe0f1e4c64a31ca28fa3245
jimao <jimao@jimaodeMBP.lan>
2022-05-27 23:30:02 +0800

    提交

--- src/ring.ts
+++ src/ring.ts
@@ -139,10 +139,8 @@ class Ring {
   private sim_base_to_swap1(in_qty: number): number {
     let out_qty = 0;
     if (this.base_market1_forward) {
-      // BTC/USDT 10
       out_qty = in_qty / this.base_market1.SellPriceEst;
     } else {
-      // USDT/BTC 0.1
       out_qty = in_qty * this.base_market1.BuyPriceEst;
     }
     return out_qty * this.fee;

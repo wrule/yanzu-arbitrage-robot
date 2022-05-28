a89e070938eb983f6ded3c19832eea5e3d503c2e
jimao <jimao@jimaodeMBP.lan>
2022-05-28 12:46:51 +0800

    提交代码

--- src/ring.ts
+++ src/ring.ts
@@ -251,7 +251,7 @@ class Ring {
         console.log('反向交易机会');
       }
 
-      console.log('Check: ', this.MarketSymbols, forward_diff_ratio, reverse_diff_ratio);
+      // console.log('Check: ', this.MarketSymbols, forward_diff_ratio, reverse_diff_ratio);
     }
   }
 }

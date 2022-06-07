0cbbfc03340e1b16634d14f5778409539416f7c9
jimao <jimao@jimaodeMacBook-Pro.local>
2022-06-07 22:13:44 +0800

    提交

--- src/mini_test.ts
+++ src/mini_test.ts
@@ -1,2 +1,25 @@
+import { Spot } from '@binance/connector';
+import secret from './.secret.json';
 
 console.log('你好，世界');
+
+async function main() {
+  const client = new Spot(
+    secret.API_KEY,
+    secret.SECRET_KEY,
+    { baseURL: 'https://api2.binance.com' },
+  );
+  const callbacks = {
+    open: () => { console.log('open') },
+    close: () => { console.log('close') },
+    message: (json_text: string) => {
+      console.log(json_text);
+    },
+  };
+  const combined_streams = client.combinedStreams(
+    ['btcusdt@@bookTicker'],
+    callbacks,
+  );
+}
+
+main();

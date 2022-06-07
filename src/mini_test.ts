63478d0cf8a3ba41d960a8aa568f22a97477550b
jimao <jimao@jimaodeMBP.lan>
2022-06-07 23:24:54 +0800

    提交

--- src/mini_test.ts
+++ src/mini_test.ts
@@ -1,7 +1,20 @@
 import { Spot } from '@binance/connector';
+import { AxiosResponse } from 'axios';
 import secret from './.secret.json';
+import { Market } from './market';
+import { TransactionResultBuy } from './transaction_result';
 
 console.log('你好，世界');
+
+
+async function load_markets(client: any) {
+  const rsp: AxiosResponse<any, any> = await client.exchangeInfo();
+  const markets = (rsp.data.symbols as any[])
+    .map((item) => new Market(client, item));
+  return new Map<string, Market>(
+    markets.map((market) => [market.symbol, market])
+  );
+}
 
 async function main() {
   const client = new Spot(
@@ -9,12 +22,15 @@ async function main() {
     secret.SECRET_KEY,
     { baseURL: 'https://api2.binance.com' },
   );
+  const market_map = await load_markets(client);
+  const marker = market_map.get('BTCUSDT') as Market;
   let time = -1;
   let count = 0;
+  let result!: TransactionResultBuy;
   const callbacks = {
     open: () => { console.log('open') },
     close: () => { console.log('close') },
-    message: (json_text: string) => {
+    message: async (json_text: string) => {
       try {
         if (time < 0) {
           time = Number(new Date());
@@ -29,8 +45,12 @@ async function main() {
           const [ask, bid] = [data.a, data.b];
           if (count % 2) {
             console.log('卖', ask, bid);
+            const sell_result = await marker.Sell(result.OutQuantity);
+            sell_result.Display();
           } else {
             console.log('买', ask, bid);
+            result = await marker.Buy(15);
+            result.Display();
           }
           count++;
         }
